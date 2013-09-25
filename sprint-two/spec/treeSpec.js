86355e69b138f2be619bf8b02e03e125754c7bd2
Peter Hayes <peter.k.hayes@gmail.com>
2013-09-25 15:41:55 -0700

    Specs for tree written.

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -11,5 +11,34 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
+  it("should not have value or children if not set", function() {
+    expect(tree.value).toEqual(undefined);
+    expect(tree.children).toEqual(undefined);
+  });
+
+  it("should be able to add children", function() {
+    var child1 = makeTree();
+    var child2 = makeTree();
+    tree.addChild(child1);
+    tree.addChild(child2);
+    expect(tree.children.length).toEqual(2);
+  });
+
+  it("should be able to search subchildren with contains", function() {
+    tree.value = 1;
+    var child1 = makeTree();
+    child1.value = 2;
+    var child2 = makeTree();
+    child2.value = 3;
+    tree.addChild(child1);
+    child1.addChild(child2);
+    expect(tree.contains(1)).toEqual(true);
+    expect(tree.contains(2)).toEqual(true);
+    expect(tree.contains(3)).toEqual(true);
+    expect(tree.contains(4)).toEqual(false);
+    expect(tree.contains(null)).toEqual(false);
+    expect(tree.contains(undefined)).toEqual(false);
+  });
+
   // Add more tests here to test the functionality of tree.
 });
