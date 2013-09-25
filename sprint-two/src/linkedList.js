a8e3e2d50b27cd9bf14b2564e7b12b729668752a
Peter Hayes <peter.k.hayes@gmail.com>
2013-09-25 15:21:10 -0700

    Adding removeHead.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,7 +3,7 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
-  
+
   list.addToTail = function(value){
     var newNode = makeNode(value);
     if (this.head) {
@@ -15,6 +15,20 @@ var makeLinkedList = function(){
     }
   };
 
+  list.removeHead = function(){
+    var result = this.head;
+    if (result) {
+      var current = this.tail;
+      while (current.next && current.next !== this.head) {
+        current = current.next;
+      }
+      this.head = current;
+      this.head.next = null;
+      return result.value;
+    } else {
+      return null;
+    }
+  };
 
   return list;
 };
