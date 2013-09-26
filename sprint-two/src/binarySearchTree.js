f12f9fed09118ab9bd332f4191451863e7eeebe9
Peter Hayes <peter.k.hayes@gmail.com>
2013-09-25 23:57:38 -0700

    Adding Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,36 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(val) {
+  return {
+    value: val,
+    parent: null,
+    left: null,
+    right: null,
+    insert: function (toAdd) {
+      var direction = (toAdd <= this.value ? "left" : "right");
+      if (this[direction]) {
+        this[direction].insert(toAdd);
+      } else {
+        this[direction] = makeBinarySearchTree(toAdd);
+      }
+    },
+    contains: function (target) {
+      if (target === this.value) {
+        return true;
+      }
+      var direction = (target < this.value ? "left" : "right");
+      if (this[direction]) {
+        return this[direction].contains(target);
+      } else {
+        return false;
+      }
+    },
+    depthFirstLog: function (iterator) {
+      if (this.left) {
+        this.left.depthFirstLog(iterator);
+      }
+      iterator(this.value);
+      if (this.right) {
+        this.right.depthFirstLog(iterator);
+      }
+    }
+  };
 };
